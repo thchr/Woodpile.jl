fe559e69f57833c81bcc6ef9a220b82ea4e8df48
thchr <tchr@mit.edu>
2025-04-15 13:32:16 +0200

    use the new cylinder-polygon intersection method to perform the "cylinder-in-cell" checks & clean up implementation
    - drop the old `Box` and `Rect` method
    
    - for now, this means we've broken the `Sphere` method, since it now lacks a working intersection check (similar problem as `Cylinder`, although the polygon-sphere intersection is probably easier - but also, the `Sphere` should sometimes now be included even if it doesn't intersect a facet: it's finite, so could be entirely inside the cell)
    
    - try to be careful about which things are "naturally" in cartesian/lattice basis
    
    - add dependencies on Bravais and Brillouin + add compat for Crystalline

--- src/Woodpile.jl
+++ src/Woodpile.jl
@@ -3,18 +3,23 @@ module Woodpile
 # ---------------------------------------------------------------------------------------- #
 
 using Crystalline: SymOperation, rotation, translation, isapproxin, @S_str, compose
-using StaticArrays
-using LinearAlgebra
+using Bravais: DirectBasis, cartesianize, transform, latticize
+using Brillouin: Cell, Brillouin, setting
+
+using StaticArrays: SVector, StaticVector
+using LinearAlgebra: norm, normalize, dot, ×
 
 import Base: minimum, maximum, *, ==, isapprox
+import Bravais: cartesianize, latticize
 
 # ---------------------------------------------------------------------------------------- #
 
-export Cylinder, Line, Box, Sphere
+export Cylinder, Line, Sphere
 export center, axis, radius
 export symmetrize
 
-# ---------------------------------------------------------------------------------------- #
+
+## --------------------------------------------------------------------------------------- #
 
 struct Line
     cntr::SVector{3,Float64}
@@ -26,50 +31,34 @@ struct Line
     end
 end
 Line(cntr, axis) = Line(convert(SVector{3,Float64}, cntr), convert(SVector{3,Float64}, axis))
-center(l::Line) = l.cntr
-axis(l::Line) = l.axis
-# Rodrigues' rotation formula (rotate a line around a unit vector n by an angle θ)
+center(l::Line)  = l.cntr
+axis(l::Line)    = l.axis
+cartesianize(l::Line, basis::DirectBasis{3}) = Line(center(l)'*basis, axis(l)'*basis)
+latticize(l::Line, basis::DirectBasis{3})    = Line(center(l)\basis,  axis(l)\basis)
 function rotate(l::Line, n::StaticVector{3}, θ::Real)
+    # Rodrigues' rotation formula (rotate a line around a unit vector n by an angle θ)
     s, c = sincos(θ)
     cntr′::SVector{3,Float64} = c*center(l) + s*(n×center(l)) + (1-c)*(n⋅center(l)) * n
     axis′::SVector{3,Float64} = c*axis(l) + s*(n×axis(l)) + (1-c)*(n⋅axis(l)) * n
     return Line(cntr′, axis′)
 end
 
+# ---------------------------------------------------------------------------------------- #
+
 struct Cylinder
     line::Line
     radius::Float64
 end
 Cylinder(cntr::AbstractVector{<:Real}, axis::AbstractVector{<:Real}, radius::Real) = Cylinder(Line(cntr, axis), radius)
-line(c::Cylinder)    = c.line
+line(c::Cylinder)   = c.line
 center(c::Cylinder) = center(line(c))
 axis(c::Cylinder)   = axis(line(c))
 radius(c::Cylinder) = c.radius
+cartesianize(c::Cylinder, basis::DirectBasis{3}) = Cylinder(transform(line(c), basis), radius(c))
+latticize(c::Cylinder, basis::DirectBasis{3})    = Cylinder(transform(line(c), basis), radius(c))
 rotate(c::Cylinder, n::StaticVector{3}, θ::Real) = Cylinder(rotate(line(c), n, θ), radius(c))
 
-struct Box
-    # axis aligned box
-    cntr::SVector{3,Float64} # center
-    width::SVector{3,Float64}
-end
-Box() = Box(SVector{3,Float64}(0.0, 0.0, 0.0))
-Box(cntr::AbstractVector) = Box(cntr, SVector{3,Float64}(1.0, 1.0, 1.0))
-center(b::Box) = b.cntr
-width(b::Box)  = b.width
-minimum(b::Box) = b.cntr .- width(b)./2
-maximum(b::Box) = b.cntr .+ width(b)./2
-
-struct Rect
-    # a 2D rectangle with sides of length 1
-    cntr::SVector{3,Float64}
-    normal::SVector{3,Float64}
-end
-
-function facets(b::Box)
-    normals = SVector{3, Float64}.(((1,0,0), (-1,0,0), (0,1,0), (0,-1,0), (0,0,1), (0,0,-1)))
-    cntrs = Ref(center(b)) .+ normals./2
-    return Rect.(cntrs, normals)
-end
+# ---------------------------------------------------------------------------------------- #
 
 struct Sphere
     cntr::SVector{3,Float64}
@@ -87,12 +76,11 @@ function (*)(op::SymOperation{3}, c::Cylinder)
     return Cylinder(Line(cntr′, axis′), radius(c))
 end
 function (==)(c1::Cylinder, c2::Cylinder)
-    # two cylinders' axes are the same if they are colinear
+    # axes are the same if they are colinear
     iszero(axis(c1) × axis(c2)) || return false
-    # two aligned cylinders' are the same if their centers lie on the line spanned
-    # by their axis
+    # lines are the same if their centers lie on the line spanned by their axis
     iszero((center(c1) - center(c2)) × axis(c1)) || return false    
-    # two cylinders are the same if they have the same radius
+    # finally, cylinders are the same if they have the same radius
     return radius(c1) == radius(c2)
 end
 function isapprox(c1::Cylinder, c2::Cylinder;
@@ -118,6 +106,7 @@ end
 Base.:+(s::Sphere, v::StaticVector{3}) = Sphere(center(s)+v, radius(s))
 
 # ---------------------------------------------------------------------------------------- #
+
 function distance_to_cylinder_axis(r::AbstractVector{<:Real}, c::Cylinder)
     # cf. https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Vector_formulation
     tmp = r - center(c)
@@ -142,110 +131,96 @@ function signed_distance(r::AbstractVector{<:Real}, s::Sphere)
     return norm(r - center(s)) - radius(s)
 end
 
-# ---------------------------------------------------------------------------------------- #
+## --------------------------------------------------------------------------------------- #
 
-struct LineBoxIntersection
-    bool::Bool
-    tmin::Float64
-    tmax::Float64
-end
-Base.Bool(i::LineBoxIntersection) = i.bool
-
-function intersects(b::Box, l::Line)
-    # branchless line-box intersection following https://tavianator.com/2011/ray_box.html
-    b_min, b_max = minimum(b), maximum(b)
-    l_cntr = center(l)
-    l_axis = axis(l)
-    inv_r_axis = 1 ./ l_axis
-
-    tx1 = (b_min[1] - l_cntr[1]) * inv_r_axis[1]
-    tx2 = (b_max[1] - l_cntr[1]) * inv_r_axis[1]
-    tmin = min(tx1, tx2)
-    tmax = max(tx1, tx2)
-
-    ty1 = (b_min[2] - l_cntr[2]) * inv_r_axis[2]
-    ty2 = (b_max[2] - l_cntr[2]) * inv_r_axis[2]
-    tmin = max(tmin, min(ty1, ty2))
-    tmax = min(tmax, max(ty1, ty2))
-    
-    tz1 = (b_min[3] - l_cntr[3]) * inv_r_axis[3]
-    tz2 = (b_max[3] - l_cntr[3]) * inv_r_axis[3]   
-    tmin = max(tmin, min(tz1, tz2))
-    tmax = min(tmax, max(tz1, tz2))
-
-    return LineBoxIntersection(tmax ≥ tmin, tmin, tmax)
-end
-function might_intersects(b::Box, c::Cylinder)
-    # poor man's intersection check: if a cylinder's center doesn't penetrate the faces
-    # of a box which is padded by the cylinder's radius, then it cannot possibly intersect
-    # the box itself (it might still not intersect the box, however).
-    # to do better than this, we should check if the cylinder itself ever intersects with
-    # the edge-segments of the box (but that is tedious... so we just make do with this)
-    b′ = Box(center(b), width(b) .+ radius(c)*2)
-    return intersects(b′, line(c))
-end
-might_intersects(c::Cylinder, b::Box=Box()) = might_intersects(b, c)
+include("cylinder_polygon_intersect.jl")
 
 # ---------------------------------------------------------------------------------------- #
 
-struct SphereBoxIntersection
-    bool::Bool
-end
-Base.Bool(i::SphereBoxIntersection) = i.bool
-
+#=
 function intersects(b::Box, s::Sphere)
     b_min, b_max = minimum(b), maximum(b)
     s_cntr = center(s)
     r = radius(s)
     for i in 1:3
-        s_cntr[i] + r - b_min[i] > 0.0 || return SphereBoxIntersection(false)
-        s_cntr[i] - r - b_max[i] < 0.0 || return SphereBoxIntersection(false)
+        s_cntr[i] + r - b_min[i] > 0.0 || return false
+        s_cntr[i] - r - b_max[i] < 0.0 || return false
     end
-    return SphereBoxIntersection(true)
+    return true
 end
-might_intersects(b::Box, s::Sphere) = intersects(b, s)
-might_intersects(s::Sphere, b::Box=Box()) = might_intersects(b, s)
+=#
 
 # ---------------------------------------------------------------------------------------- #
+
+function facets(uc :: Cell) # NB: we always return in _cartesian_ coordinates
+    vs = Brillouin.vertices(uc)
+    vs = if Brillouin.setting(uc) == Brillouin.LATTICE
+        cartesianize.(vs, Ref(Brillouin.basis(uc))) # non-mutating, intentionally
+    end
+    return [vs[f] for f in Brillouin.faces(uc)]
+end
+
+function facets(Vs :: DirectBasis{3}; origin::SVector{3,Float64}=SVector(0.0,0.0,0.0))
+    # returns a vector of vertices (i.e., faces, ordered counter-clockwise, i.e., with 
+    # normal point "outward") of the unit cell's facets
+    # - `origin`: where to place the center of the unit cell
+    z = origin - sum(Vs) / 2
+    A1 = [z, z + Vs[3], z + Vs[2] + Vs[3], z + Vs[2]]
+    A2 = [z, z + Vs[1], z + Vs[3] + Vs[1], z + Vs[3]]
+    A3 = [z, z + Vs[2], z + Vs[1] + Vs[2], z + Vs[1]]
+    B1 = reverse(A1) .+ (Vs[1],)
+    B2 = reverse(A2) .+ (Vs[2],)
+    B3 = reverse(A3) .+ (Vs[3],)
+    return [A1, A2, A3, B1, B2, B3]
+end
+# ---------------------------------------------------------------------------------------- #
+
 const NEIGHBOR_TRANSLATIONS = 
     NTuple{26, SVector{3, Int}}([SVector(I[1],I[2],I[3]) for I in vec(CartesianIndices((-1:1, -1:1, -1:1))) if !iszero(I)])
 
-function symmetrize(ops::AbstractVector{SymOperation{3}},
-            cs::Union{AbstractVector{Cylinder}, AbstractVector{Sphere}};
-            add_neighbors::Bool=true)
+function symmetrize(
+    ops::AbstractVector{SymOperation{3}},
+    cs::Union{Cylinder, Sphere, AbstractVector{Cylinder}, AbstractVector{Sphere}},
+    boundary::Union{DirectBasis{3}, Cell{3}},
+    fs::AbstractVector{<:AbstractVector{<:StaticVector{3, Float64}}} = facets(boundary);
+    add_neighbors::Bool=true,
+    cartesian_ops::Bool=false
+)
+    Rs = if boundary isa DirectBasis{3}
+        boundary
+    elseif boundary isa Cell{3}
+        DirectBasis{3}(Brillouin.basis(boundary))
+    else
+        error("unreachable: `boundary` is not a `DirectBasis` or `Cell`")
+    end
 
+    cs isa AbstractVector || (cs = [cs])
     cs′ = copy(cs)
     for op in ops
+        opᶜ = cartesian_ops ? op : cartesianize(op, Rs)
         for c in cs
-            c′ = op * c
-            (!isapproxin(c′, cs′) && Bool(might_intersects(c′))) && push!(cs′, c′)
-            if add_neighbors
-                for n in NEIGHBOR_TRANSLATIONS
-                    c′′ = c′ + n
-                    (!isapproxin(c′′, cs′) && Bool(might_intersects(c′′))) && push!(cs′, c′′)
-                end
+            c′ = opᶜ * c
+            isapproxin(c′, cs′) && continue # already seen
+            any(f -> intersects(c′, f), fs) && push!(cs′, c′)
+            
+            add_neighbors || continue
+            for t in NEIGHBOR_TRANSLATIONS
+                c′′ = c′ + cartesianize(t, Rs)
+                isapproxin(c′′, cs′) && continue # already seen
+                any(f -> intersects(c′′, f), fs) && push!(cs′, c′′)
             end
         end
     end
 
     if length(cs′) == length(cs)
-        # no extra objects were added, we converged       
+        # no new objects added ⇒ converged       
         # however, before returning we do the following check: in case the initial seed
         # point was outside the unit cell, there might be an element in `cs′` that really
         # doesn't intersect the unitcell; no point in keeping it around - filter it out here
-        return filter!(c->Bool(might_intersects(c)), cs′) # no extra cylinders were added, we converged
+        return filter!(c->any(f->intersects(c, f), fs), cs′) # no new cylinders added ⇒ converged
     else
-        return symmetrize(ops, cs′; add_neighbors=false)
+        return symmetrize(ops, cs′, boundary, fs; add_neighbors=false, cartesian_ops)
     end
-end
-symmetrize(ops::AbstractVector{SymOperation{3}}, c::Union{Cylinder, Sphere}; kwargs...) = symmetrize(ops, [c]; kwargs...)
-
-function box_boundary(r, limits, boxtol=1e-8)
-    x,y,z = r
-    (x < limits[1]+boxtol || x > limits[2]-boxtol) && return 1e20
-    (y < limits[3]+boxtol || y > limits[4]-boxtol) && return 1e20
-    (z < limits[5]+boxtol || z > limits[6]-boxtol) && return 1e20
-    return -1e20
 end
 
 # ---------------------------------------------------------------------------------------- #
